cdb7d0f75513ea5e9598c4cde6347dbdb64e4c9d
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-02 14:40:52 +0200

    [UPD] Snack 1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,6 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-
+names.forEach((element) => {
+    console.log(element); 
+})
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
