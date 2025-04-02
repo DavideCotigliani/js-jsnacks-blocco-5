d80e01b2dc386ae154affafc779d3572c40e31aa
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-02 14:54:05 +0200

    [UPD] snack 4 completed

--- snack-4/script.js
+++ snack-4/script.js
@@ -4,6 +4,10 @@ const posts = [
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
+const newPosts = posts.map((item)=>{
+return item.author
+})
+console.log(newPosts);
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
